c7d52ca0a48f178eb1e24c9ebbdcc24f241ace39
Dennis <dennismaltez463@gmail.com>
2023-12-01 13:37:04 -0600

    0.1.1

--- js/codigo.js
+++ js/codigo.js
@@ -15,17 +15,17 @@ animationImgClass : document.querySelector('.animationImgClass'),
 containerTxtConsoles : document.querySelector('.containerTxtConsoles')
 }
 
-window.addEventListener('load',()=> {
-objD.sectionWelcome.style.opacity = '0';
-objD.sectionInit.style.opacity = '0';
-setTimeout(()=>{objD.sectionWelcome.style.display = 'none'; inti()},1000)
-})
+// window.addEventListener('load',()=> {
+// objD.sectionWelcome.style.opacity = '0';
+// objD.sectionInit.style.opacity = '0';
+// setTimeout(()=>{objD.sectionWelcome.style.display = 'none'; inti()},1000)
+// })
 
 
-function inti () {
-	objD.sectionInit.style.display = 'block'
-setTimeout(()=>{objD.sectionInit.style.opacity = '1';},500)
-}
+// function inti () {
+// 	objD.sectionInit.style.display = 'block'
+// setTimeout(()=>{objD.sectionInit.style.opacity = '1';},500)
+// }
 
 // scroll console
 // config
